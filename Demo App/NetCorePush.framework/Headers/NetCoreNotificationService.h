82e662f1ced264d37d71a0b75aa58b348dba25c2
Jobin Kurian <jobin.kurian@netcore.co.in>
2020-06-03 00:54:38 +0530

    v2.5.2

--- Demo App/NetCorePush.framework/Headers/NetCoreNotificationService.h
+++ Demo App/NetCorePush.framework/Headers/NetCoreNotificationService.h
@@ -5,7 +5,7 @@
  
  @author Netcore Solutions
  @copyright  2019 Netcore Solutions
- @version    2.4.5 */
+ @version    2.5.2 */
 
 #import <UserNotifications/UserNotifications.h>
 #import <UserNotifications/UNNotificationContent.h>
