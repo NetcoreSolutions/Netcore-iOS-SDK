27d633007af99244f07d512a05b4c37b2d525465
Jobin Kurian <jobin.kurian@netcore.co.in>
2020-08-08 11:52:58 +0530

    v2.5.5

--- Demo App/NetCorePush.framework/Headers/NetCoreInstallation.h
+++ Demo App/NetCorePush.framework/Headers/NetCoreInstallation.h
@@ -5,7 +5,7 @@
  
  @author Netcore Solutions
  @copyright  2019 Netcore Solutions
- @version    2.4.8 */
+ @version    2.5.5 */
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import "NetCoreConstant.h"
